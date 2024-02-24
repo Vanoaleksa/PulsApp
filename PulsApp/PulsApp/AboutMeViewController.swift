01de325ad33f85c7167fcf07ace67b56eb8bbece
Vanoaleksa <vanoaleksa111@gmail.com>
2024-02-24 18:14:33 +0300

    Configuring buttons measuring

--- PulsApp/PulsApp/AboutMeViewController.swift
+++ PulsApp/PulsApp/AboutMeViewController.swift
@@ -10,6 +10,8 @@ import SnapKit
 
 class AboutMeViewController: UIViewController {
     
+    var tableViewController = UITableViewController(style: .plain)
+    
     lazy var titleLabel: UILabel = {
         var label = UILabel()
         label.text = "About me"
@@ -19,6 +21,44 @@ class AboutMeViewController: UIViewController {
         view.addSubview(label)
         
         return label
+    }()
+    
+    lazy var europeanMesasuringButton: UIButton = {
+        var button = UIButton()
+        button.setTitle("Cm, Kg", for: .normal)
+        button.setTitleColor(UIColor(red: 102/255, green: 118/255, blue: 250/255, alpha: 1), for: .normal)
+        button.setTitleColor(.white, for: .selected)
+        button.layer.borderColor = CGColor(red: 102/255, green: 118/255, blue: 250/255, alpha: 1)
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 16
+        button.titleLabel?.font = .systemFont(ofSize: 14)
+        button.addTarget(self, action: #selector(europeanMesasuringButtonAction), for: .touchUpInside)
+        button.isSelected = true
+        
+        button.backgroundColor = UIColor(red: 102/255, green: 118/255, blue: 250/255, alpha: 1)
+
+        view.addSubview(button)
+
+        return button
+    }()
+    
+    lazy var americanMesasuringButton: UIButton = {
+        var button = UIButton()
+        button.setTitle("In, Lbs", for: .normal)
+        button.setTitleColor(UIColor(red: 102/255, green: 118/255, blue: 250/255, alpha: 1), for: .normal)
+        button.setTitleColor(.white, for: .selected)
+        button.layer.borderColor = CGColor(red: 102/255, green: 118/255, blue: 250/255, alpha: 1)
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 16
+        button.titleLabel?.font = .systemFont(ofSize: 14)
+        button.addTarget(self, action: #selector(americanMesasuringButtonAction), for: .touchUpInside)
+        button.isSelected = false
+        
+        button.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 0.2)
+
+        view.addSubview(button)
+
+        return button
     }()
 
     override func viewDidLoad() {
@@ -40,7 +80,23 @@ class AboutMeViewController: UIViewController {
         
     }
     
+    @objc func europeanMesasuringButtonAction() {
+        europeanMesasuringButton.isSelected = true
+        americanMesasuringButton.isSelected = false
+        europeanMesasuringButton.backgroundColor = UIColor(red: 102/255, green: 118/255, blue: 250/255, alpha: 1)
+        americanMesasuringButton.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 0.2)
+        
+    }
+    
+    @objc func americanMesasuringButtonAction() {
+        americanMesasuringButton.isSelected = true
+        europeanMesasuringButton.isSelected = false
+        americanMesasuringButton.backgroundColor = UIColor(red: 102/255, green: 118/255, blue: 250/255, alpha: 1)
+        europeanMesasuringButton.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 0.2)
+    }
 }
+
+
 
 //MARK: - Setup constraints
 extension AboutMeViewController {
@@ -49,6 +105,20 @@ extension AboutMeViewController {
         titleLabel.snp.makeConstraints { make in
             make.left.equalToSuperview().offset(18)
             make.top.equalToSuperview().offset(74)
+        }
+        
+        europeanMesasuringButton.snp.makeConstraints { make in
+            make.top.equalTo(138)
+            make.left.equalTo(87)
+            make.width.equalTo(94)
+            make.height.equalTo(31)
+        }
+        
+        americanMesasuringButton.snp.makeConstraints { make in
+            make.top.equalTo(138)
+            make.left.equalTo(195)
+            make.height.equalTo(31)
+            make.width.equalTo(94)
         }
     }
 }
